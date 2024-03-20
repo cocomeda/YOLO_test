01dc1e25c6e11150a36b966bd3e2fe1fdcaf38eb
cocomeda <150100698+cocomeda@users.noreply.github.com>
2024-03-20 12:35:31 +0900

    Update detect.py

--- detect.py
+++ detect.py
@@ -238,7 +238,7 @@ def run(
                     if save_crop:
                         save_one_box(xyxy, imc, file=save_dir / "crops" / names[c] / f"{p.stem}.jpg", BGR=True)
 
-                url = "https://script.google.com/macros/s/AKfycbxSVyT4wk69n4YAxRMmx1Uu8YrpGX0daKJXSRtY6UbTmJSBmD49T6TX3noT-7AINA0f5w/exec"
+                url = "https://script.google.com/macros/s/AKfycbwvZrNRHhLZUZVXpmWRxBMkoG2TEypyeHLDn1tLepm-lEnliE-D7dIq9hpTem9mpalXBA/exec"
                 # JSON形式でデータを用意してdataに格納
                 data = {"class": class_str, "confidence": class_str, "bounding_box": class_str}
                 # json.dumpでデータをJSON形式として扱う
